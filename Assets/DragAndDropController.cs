dd14465f37775d366f9a02c6d4e04e0e5a22ed02
michael <michaelzixizhoubusiness@gmail.com>
2022-11-10 01:40:17 -0500

    grid system

---
+++ Assets/DragAndDropController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDropController : MonoBehaviour
+{   
+    public bool snapToGrid = true;
+    public bool smartDrag = true;
+    public bool isDraggable = true;
+    public bool isDragged = false;
+    Vector2 initialPositionMouse;
+    Vector2 initialPositionObject;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDragged){
+            if (!smartDrag){
+                transform.position = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            } else {
+                transform.position = initialPositionMouse + (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - initialPositionMouse;
+            }
+            if (snapToGrid) {
+                transform.position = new Vector2(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.y));
+            }
+        }
+    }
+
+    private void OnMouseOver() {
+        if (isDraggable && Input.GetMouseButtonDown(0)) {
+            if (smartDrag) {
+                initialPositionMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                initialPositionObject = transform.position;
+            }
+            isDragged = true;
+        }
+    }
+
+    private void OnMouseUp() {
+        isDragged = false;
+    }
+}
