dc2e8ecf6936bf48efe461023a64cd794f8194f8
michael <michaelzixizhoubusiness@gmail.com>
2022-11-13 15:36:48 -0500

    comments

--- Assets/Scripts/Managers/UnitManager.cs
+++ Assets/Scripts/Managers/UnitManager.cs
@@ -2,6 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+This will manage unit spawning, both for allies and for enemies
+
+This will be called by GameManager to perform said actions
+*/
 public class UnitManager : MonoBehaviour
 {
     // Start is called before the first frame update
